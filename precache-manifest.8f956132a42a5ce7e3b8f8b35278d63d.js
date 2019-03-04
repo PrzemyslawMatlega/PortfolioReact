2f34cbdc0e225838d8846d32708f5521e9ba599f
PrzemyslawMatlega <przemekmat@yahoo.com>
2019-03-04 10:02:37 +0100

    Updates

---
+++ precache-manifest.8f956132a42a5ce7e3b8f8b35278d63d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4c945428c725531f4b63207b6431325",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/left.a4c94542.svg"
+  },
+  {
+    "revision": "5a12ac14aab6a4a2cb21",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/css/main.470baa7c.chunk.css"
+  },
+  {
+    "revision": "028a536bca26dfaae7c6",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/js/runtime~main.028a536b.js"
+  },
+  {
+    "revision": "7692734278d187289964",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/js/2.76927342.chunk.js"
+  },
+  {
+    "revision": "4bb621104d11e2b422a632c65cb5d190",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/Hello-World.4bb62110.mp4"
+  },
+  {
+    "revision": "8eefe61d991dee9d89e9971893a8454b",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/airplane.8eefe61d.svg"
+  },
+  {
+    "revision": "5a12ac14aab6a4a2cb21",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/js/main.5a12ac14.chunk.js"
+  },
+  {
+    "revision": "b09fbefb77f129efddb334d56948f57e",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/right.b09fbefb.svg"
+  },
+  {
+    "revision": "06c38e601a7781e47ddc86de0426e3aa",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/facebook.06c38e60.svg"
+  },
+  {
+    "revision": "154629f25d417c8c0a4ac3a83707979a",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/linkedin.154629f2.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "4c74fb332076ea27a118e042c8906276",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/static/media/envelope.4c74fb33.svg"
+  },
+  {
+    "revision": "11f5c61b9a611e12b387de183e7b0fc4",
+    "url": "/PrzemyslawMatlega/PortfolioReact/tree/gh-pages/index.html"
+  }
+];
